e2f547390fac28dfddbc6dc8c2aef1f00c7ec4ba
GuiParpineli <guiparpineli@hotmail.com>
2023-11-02 18:12:19 -0300

    removido material e padrao agora lumo design

--- src/main/java/com/clearcont/clearcontapp/ClearContAppApplication.java
+++ src/main/java/com/clearcont/clearcontapp/ClearContAppApplication.java
@@ -3,12 +3,13 @@ package com.clearcont.clearcontapp;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import com.vaadin.flow.theme.material.Material;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@Theme(themeClass = Material.class, variant = Material.DARK)
+@Theme(themeClass = Lumo.class)
 @PWA(
         name = "ClearCont",
         shortName = "CC",
