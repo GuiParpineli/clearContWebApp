f5994379bad4e82ccec5bfe02ab23c72817c9736
GuiParpineli <guiparpineli@hotmail.com>
2024-01-07 23:38:20 -0300

    addicionado build

--- src/main/java/com/clearcont/clearcontapp/DataLoader.java
+++ src/main/java/com/clearcont/clearcontapp/DataLoader.java
@@ -30,89 +30,98 @@ public class DataLoader implements ApplicationRunner {
     
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        Empresa empresa = new Empresa();
+        Empresa empresa2 = new Empresa();
+        if (empresaGroupRepository.findAll().isEmpty()) {
+            
+            empresa = empresaRepository.save(
+                    new Empresa(
+                            null,
+                            "Empresa A ",
+                            "01.001.001/0001-01",
+                            "cientea@gmail.com"
+                    ));
+            
+            empresa2 = empresaRepository.save(
+                    new Empresa(
+                            null,
+                            "Empresa b ",
+                            "02.001.001/0001-01",
+                            "cienteb@gmail.com"
+                    ));
+        }
+        if (balanceteRepository.findAll().isEmpty()) {
+            balanceteRepository.save(Balancete.builder().classificacao("ATIVO")
+                    .empresa(empresa)
+                    .id(null)
+                    .nomeConta("BANCO ITAU")
+                    .numeroConta(375)
+                    .mes("JANEIRO")
+                    .ano(2024)
+                    .totalBalancete(248.36334).build());
+            
+            balanceteRepository.save(Balancete.builder().classificacao("ATIVO")
+                    .empresa(empresa)
+                    .id(null)
+                    .nomeConta("BANCO ITAU")
+                    .numeroConta(375)
+                    .mes("JANEIRO")
+                    .ano(2024)
+                    .totalBalancete(248.36334).build());
+            
+            balanceteRepository.save(Balancete.builder().classificacao("ATIVO")
+                    .empresa(empresa2)
+                    .id(null)
+                    .nomeConta("BANCO BRADESCO")
+                    .numeroConta(379)
+                    .ano(2023)
+                    .mes("DEZEMBRO")
+                    .totalBalancete(9948.36334).build());
+        }
+        if (controleRepository.findAll().isEmpty()) {
+            controleRepository.save(
+                    new Controle(
+                            null,
+                            "ATIVO",
+                            "Sim",
+                            "BRADESCO",
+                            10.219,
+                            100.0,
+                            99.00,
+                            "STATUS",
+                            "Obervacoes ",
+                            true,
+                            false,
+                            LocalDate.now(),
+                            null,
+                            empresa
+                    )
+            );
+        }
+        if (empresaGroupRepository.findAll().isEmpty()) {
+            empresaGroupRepository.save(
+                    new EmpresaGroup(
+                            0,
+                            "EmpresaGrande",
+                            "12345",
+                            "empresaGrande@email.com",
+                            empresaRepository.findAll())
+            );
+        }
         
-        Empresa empresa = empresaRepository.save(
-                new Empresa(
-                        null,
-                        "Empresa A ",
-                        "01.001.001/0001-01",
-                        "cientea@gmail.com"
-                ));
-        
-        Empresa empresa2 = empresaRepository.save(
-                new Empresa(
-                        null,
-                        "Empresa b ",
-                        "02.001.001/0001-01",
-                        "cienteb@gmail.com"
-                ));
-        
-        balanceteRepository.save(Balancete.builder().classificacao("ATIVO")
-                .empresa(empresa)
-                .id(null)
-                .nomeConta("BANCO ITAU")
-                .numeroConta(375)
-                .mes("JANEIRO")
-                .ano(2024)
-                .totalBalancete(248.36334).build());
-        
-        balanceteRepository.save(Balancete.builder().classificacao("ATIVO")
-                .empresa(empresa)
-                .id(null)
-                .nomeConta("BANCO ITAU")
-                .numeroConta(375)
-                .mes("JANEIRO")
-                .ano(2024)
-                .totalBalancete(248.36334).build());
-        
-        balanceteRepository.save(Balancete.builder().classificacao("ATIVO")
-                .empresa(empresa2)
-                .id(null)
-                .nomeConta("BANCO BRADESCO")
-                .numeroConta(379)
-                .ano(2023)
-                .mes("DEZEMBRO")
-                .totalBalancete(9948.36334).build());
-        
-        controleRepository.save(
-                new Controle(
-                        null,
-                        "ATIVO",
-                        "Sim",
-                        "BRADESCO",
-                        10.219,
-                        100.0,
-                        99.00,
-                        "STATUS",
-                        "Obervacoes ",
-                        true,
-                        false,
-                        LocalDate.now(),
-                        null,
-                        empresa
-                )
-        );
-        
-        empresaGroupRepository.save(
-                new EmpresaGroup(
-                        0,
-                        "EmpresaGrande",
-                        "12345",
-                        "empresaGrande@email.com",
-                        empresaRepository.findAll())
-        );
-        
-        composicaoLancamentosContabeisRepository.save(
-                new ComposicaoLancamentosContabeis(
-                        0,
-                        LocalDate.now(),
-                        "loreash",
-                        100.0,
-                        200.0,
-                        (100.0 - 200.0),
-                        "EM ABERTO",
-                        balanceteRepository.findAll().getFirst()
-                )
-        );
+        if (composicaoLancamentosContabeisRepository.findAll().isEmpty()) {
+            composicaoLancamentosContabeisRepository.save(
+                    new ComposicaoLancamentosContabeis(
+                            0,
+                            LocalDate.now(),
+                            "loreash",
+                            100.0,
+                            200.0,
+                            (100.0 - 200.0),
+                            "EM ABERTO",
+                            balanceteRepository.findAll().getFirst()
+                    )
+            );
+        }
     }
 }
